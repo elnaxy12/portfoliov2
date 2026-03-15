43dd03b9a951117c43c5a85fc70644d540057f62
elnaxy12 <aryagilang533@gmail.com>
2026-03-15 13:49:18 +0700

    git add .

--- app/page.tsx
+++ app/page.tsx
@@ -182,17 +182,18 @@ export default function Home() {
         <Sidebar />
         <Navbar />
 
-        <div ref={waveRef} className="sticky bottom-0 w-full overflow-hidden leading-[0] z-50 -mt-20 pointer-events-none">
-        <svg
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-          className="relative block w-full h-10 md:h-20"
+        <div
+          ref={waveRef}
+          className="sticky bottom-0 w-full overflow-hidden leading-[0] z-50 -mt-20 pointer-events-none"
         >
-          <path
-            d="M0,120 C600,0 600,0 1200,120 L1200,120 L0,120 Z"
-          />
-        </svg>
-      </div>
+          <svg
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+            className="relative block w-full h-10 md:h-20"
+          >
+            <path d="M0,120 C600,0 600,0 1200,120 L1200,120 L0,120 Z" />
+          </svg>
+        </div>
       </div>
 
       {/* Section 1: scroll → hitam ke putih */}
@@ -216,6 +217,23 @@ export default function Home() {
           learn new technologies and improve my skills in full stack
           development."
         </h1>
+      </div>
+
+      <div className="h-screen relative">
+        <div className="absolute top-0 w-full overflow-hidden leading-[0] z-50 pointer-events-none bg-black">
+          <svg
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+            className="relative block w-full h-10 md:h-20"
+            style={{ transform: "rotate(180deg)" }}
+          >
+            <path
+              d="M0,120 C600,0 600,0 1200,120 L1200,120 L0,120 Z"
+              fill="white"
+            />
+          </svg>
+        </div>
+
       </div>
     </div>
   );
